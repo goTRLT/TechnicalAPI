6a4476b9bc4fcba123b1e8ce5f504b57ffe40e38
Hursow <thuanraellombardi@gmail.com>
2025-10-02 21:46:08 -0300

    add Boleto entity

---
+++ Domain/Entities/Boleto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    public class Boleto
+    {
+        public int Id { get; set; }
+        public string PayorName { get; set; }
+        public int? PayorCPF_CNPJ { get; set; }
+        public string PayeeName { get; set; }
+        public int Amount { get; set; }
+        public DateOnly DueDate { get; set; }
+        public string? Notes { get; set; }
+        public Banco BancoId { get; set; }
+    }
+}
