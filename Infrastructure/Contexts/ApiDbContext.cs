198e187e01c58e1276ad341a7e5101cc47e798b8
Hursow <thuanraellombardi@gmail.com>
2025-10-02 21:48:51 -0300

    add ApiDbContext for EF

---
+++ Infrastructure/Contexts/ApiDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Context
+{
+    public class ApiDbContext : DbContext
+    {
+        public DbSet<Banco> Bancos => Set<Banco>();
+        public DbSet<Boleto> Boletos => Set<Boleto>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+
+            modelBuilder.HasDefaultSchema("TechnicalDb");
+        }
+    }
+}
