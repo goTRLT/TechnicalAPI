304a40890b3ce7a033f832f0673b8c45a0defbd5
Hursow <thuanraellombardi@gmail.com>
2025-10-04 20:11:26 -0300

    add ServicesExtensions

---
+++ Api/ServiceExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Application.Interfaces;
+using Application.Services;
+using Domain.Interfaces;
+using Infrastructure.Context;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api
+{
+    public static class ServiceExtensions
+    {
+        public static void AddServices(this IServiceCollection services, string connectionString)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            services
+                .AddEntityFrameworkNpgsql()
+                .AddDbContext<TechDbContext>(options => options
+                .UseNpgsql(connectionString));
+
+            services.AddScoped<IBancoService, BancoService>();
+            services.AddScoped<IBoletoService, BoletoService>();
+            services.AddScoped<IBancoRepository, BancoRepository>();
+            services.AddScoped<IBoletoRepository, BoletoRepository>();
+        }
+    }
+}
