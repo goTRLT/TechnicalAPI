cf4860b7cbf1ea1b5b36c3ea5d523bb2eb191704
Hursow <thuanraellombardi@gmail.com>
2025-10-04 20:06:54 -0300

    add IBancoRepository

---
+++ Domain/Interfaces/IBancoRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IBancoRepository
+    {
+        Task<List<Banco>> FindAll();
+        Task<Banco> Find(int code);
+        Task<bool> Create(Banco banco);
+    }
+}
