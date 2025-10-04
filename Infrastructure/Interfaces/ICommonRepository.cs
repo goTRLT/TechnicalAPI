7cd4cf951c813b2b1f1488639f527e9453434b3e
Hursow <thuanraellombardi@gmail.com>
2025-10-04 20:07:26 -0300

    add ICommonRepository

---
+++ Infrastructure/Interfaces/ICommonRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Interfaces
+{
+    public interface ICommonRepository<T> where T : class
+    {
+        Task<IList<T>> GetAll();
+        Task<T> Get(int key);
+        Task UseAsync(T entity);
+    }
+}
