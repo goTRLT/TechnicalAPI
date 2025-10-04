30e839fedb29e88aa1ecab64e702ab7fe4f43fce
Hursow <thuanraellombardi@gmail.com>
2025-10-04 20:10:24 -0300

    add BoletoService

---
+++ Application/Services/BoletoService.cs
@@ -0,0 +1,24 @@
+﻿using Application.Interfaces;
+using Domain.Entity;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class BoletoService : IBoletoService
+    {
+        public Task<Boleto> Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Create(Boleto banco)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
