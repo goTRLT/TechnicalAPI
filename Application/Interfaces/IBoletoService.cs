b397c64869a1694512e5225019c936fa709cb799
Hursow <thuanraellombardi@gmail.com>
2025-10-04 20:10:14 -0300

    add IBoletoService

---
+++ Application/Interfaces/IBoletoService.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IBoletoService
+    {
+        Task<Boleto> Find(int id);
+        Task<bool> Create(Boleto banco);
+    }
+}
