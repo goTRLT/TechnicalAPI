b31be0e7e65472d4b165c92a3ec19fc1f9452a7c
Hursow <thuanraellombardi@gmail.com>
2025-10-04 20:10:06 -0300

    add BancoService

---
+++ Application/Services/BancoService.cs
@@ -0,0 +1,29 @@
+﻿using Application.Interfaces;
+using Domain.Entity;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class BancoService : IBancoService
+    {
+        public Task<List<Banco>> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Banco> Find(int code)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Create(Banco banco)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
