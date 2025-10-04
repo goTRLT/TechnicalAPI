9e54b6d85e2b23ddf8a72b7fa4db0cc4b7d91d67
Hursow <thuanraellombardi@gmail.com>
2025-10-04 20:09:56 -0300

    add IBancoService

---
+++ Application/Interfaces/IBancoService.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IBancoService
+    {
+        Task<List<Banco>> FindAll();
+        Task<Banco> Find(int code);
+        Task<bool> Create(Banco banco);
+    }
+}
