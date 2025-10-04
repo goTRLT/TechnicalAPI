3de17c60f95f41cd49b7432f9c240aa18f959391
Hursow <thuanraellombardi@gmail.com>
2025-10-04 20:07:06 -0300

    add IBoletoRepository

---
+++ Domain/Interfaces/IBoletoRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IBoletoRepository
+    {
+        Task<Boleto> Find(int id);
+        Task<bool> Create(Boleto banco);
+    }
+}
