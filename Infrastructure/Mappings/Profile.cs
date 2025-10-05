d282993f925838c522578acdf4ad828f17ef8d6f
Hursow <thuanraellombardi@gmail.com>
2025-10-05 15:32:29 -0300

    add AutoMapper Profile

---
+++ Infrastructure/Mappings/Profile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Domain.DTOs;
+using Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Mappings
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Banco, BancoDto>().ReverseMap();
+            CreateMap<Boleto, BoletoDto>().ReverseMap();
+        }
+    }
+}
