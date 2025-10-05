880ad64d68ca43b442948657e1e34d18b088069b
Hursow <thuanraellombardi@gmail.com>
2025-10-05 15:29:13 -0300

    refactor add [] to properties

--- Domain/Entities/Boleto.cs
+++ Domain/Entities/Boleto.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,13 +10,33 @@ namespace Domain.Entity
 {
     public class Boleto
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string PayorName { get; set; }
+
+        [MaxLength(14)]
         public int? PayorCPF_CNPJ { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string PayeeName { get; set; }
+
+        [Required]
         public int Amount { get; set; }
+
+        [Required]
         public DateOnly DueDate { get; set; }
+
+        [MaxLength(50)]
         public string? Notes { get; set; }
-        public Banco BancoId { get; set; }
+
+        [Required]
+        [ForeignKey("Banco")]
+        public int BancoId { get; set; }
+
+        public Banco Banco { get; set; }
     }
 }
