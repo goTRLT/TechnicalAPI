62db6b91845943cb76231444d52b708251a060c0
Hursow <thuanraellombardi@gmail.com>
2025-10-05 15:33:35 -0300

    refactor add implementation for BoletoService

--- Application/Services/BoletoService.cs
+++ Application/Services/BoletoService.cs
@@ -1,4 +1,6 @@
 ﻿using Application.Interfaces;
+using AutoMapper;
+using Domain.DTOs;
 using Domain.Entity;
 using Domain.Interfaces;
 using System;
@@ -11,14 +13,29 @@ namespace Application.Services
 {
     public class BoletoService : IBoletoService
     {
-        public Task<Boleto> Find(int id)
+        private readonly IBoletoRepository _boletoRepository;
+        private readonly IBancoRepository _bancoRepository;
+        private readonly IMapper _mapper;
+
+        public BoletoService(IBoletoRepository boletoRepository, IBancoRepository bancoRepository, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _boletoRepository = boletoRepository;
+            _bancoRepository = bancoRepository;
+            _mapper = mapper;
         }
 
-        public Task<bool> Create(Boleto banco)
+        public async Task<BoletoDto> Find(int id)
         {
-            throw new NotImplementedException();
+            var boleto = await _boletoRepository.Find(id);
+            return _mapper.Map<BoletoDto>(boleto);
+        }
+
+        public async Task<bool> Create(BoletoDto boletoDto)
+        {
+            var boletoMap = _mapper.Map<Boleto>(boletoDto);
+            var success = await _boletoRepository.Create(boletoMap);
+            return success;
         }
     }
 }
+
