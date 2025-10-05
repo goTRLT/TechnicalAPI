7294f8081fb43d756a56e77e2586e7c049bfb55f
Hursow <thuanraellombardi@gmail.com>
2025-10-05 15:29:40 -0300

    add BancoDto

---
+++ Domain/DTOs/BancoDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTOs
+{
+    public class BancoDto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        public int Code { get; set; }
+
+        [Required]
+        public decimal InterestPercent { get; set; }
+    }
+}
