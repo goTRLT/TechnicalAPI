2e0c3878e50d0cd335a262a2787d460e4799c25b
Hursow <thuanraellombardi@gmail.com>
2025-10-05 15:30:44 -0300

    refactor add DTO reference

--- Application/Interfaces/IBoletoService.cs
+++ Application/Interfaces/IBoletoService.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Entity;
+﻿using Domain.DTOs;
+using Domain.Entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,7 @@ namespace Application.Interfaces
 {
     public interface IBoletoService
     {
-        Task<Boleto> Find(int id);
-        Task<bool> Create(Boleto banco);
+        Task<BoletoDto> Find(int id);
+        Task<bool> Create(BoletoDto bancoDto);
     }
 }
