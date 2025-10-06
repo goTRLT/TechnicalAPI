28933a23b89d5aa6ac378d507ce22091ab14fcbd
Hursow <thuanraellombardi@gmail.com>
2025-10-06 04:48:42 -0300

    Refactor CommonRepository and clean up using directives
    Refactored the `CommonRepository` class for improved readability:
    - Adjusted indentation and moved opening braces `{` inline with declarations.
    - Updated methods (`GetAll`, `Get`, `UseAsync`) for consistent formatting.
    - Enhanced `UseAsync` to explicitly add entities using `AddAsync`.
    
    Cleaned up unused `using` directives:
    - Removed unnecessary namespaces like `Domain.Entity` and `System.*`.
    - Added `Infrastructure.Interfaces` for required functionality.
    
    These changes improve code clarity and maintain functionality.

--- Infrastructure/Repositories/CommonRepository.cs
+++ Infrastructure/Repositories/CommonRepository.cs
@@ -1,38 +1,31 @@
-﻿using Domain.Entity;
-using Domain.Interfaces;
-using Infrastructure.Context;
+﻿using Infrastructure.Context;
 using Infrastructure.Interfaces;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure.Repository
-{
-    public class CommonRepository<T> : ICommonRepository<T> where T : class
     {
+    public class CommonRepository<T> : ICommonRepository<T> where T : class
+        {
         protected readonly TechDbContext _context;
 
         public CommonRepository(TechDbContext techDbContext)
-        {
+            {
             _context = techDbContext;
-        }
+            }
 
         public async Task<IList<T>> GetAll()
-        {
+            {
             return await _context.Set<T>().ToListAsync();
-        }
+            }
 
         public async Task<T> Get(int key)
-        {
+            {
             return await _context.Set<T>().FindAsync(key);
-        }
+            }
 
         public async Task UseAsync(T entity)
-        {
+            {
             await _context.Set<T>().AddAsync(entity);
+            }
         }
     }
-}
