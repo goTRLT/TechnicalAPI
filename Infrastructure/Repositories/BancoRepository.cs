76329e411e9de8a37975f5e083ed718edd65281a
Hursow <thuanraellombardi@gmail.com>
2025-10-06 04:48:19 -0300

    Refactor BancoRepository and improve method clarity
    Removed unused `using` directives to clean up the code. Renamed `Find` to `FindByCode` and updated its implementation for clarity. Added a new `FindById` method to fetch entities by `Id`. Improved formatting and readability of `FindAll` and `Create` methods. Removed redundant logic in the old `Find` method.

--- Infrastructure/Repositories/BancoRepository.cs
+++ Infrastructure/Repositories/BancoRepository.cs
@@ -1,41 +1,38 @@
 ﻿using Domain.Entity;
 using Domain.Interfaces;
 using Infrastructure.Context;
-using Infrastructure.Interfaces;
 using Infrastructure.Repository;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure.Repositories
-{
-    public class BancoRepository : CommonRepository<Banco>, IBancoRepository
     {
+    public class BancoRepository : CommonRepository<Banco>, IBancoRepository
+        {
         public BancoRepository(TechDbContext techDbContext) : base(techDbContext) { }
 
         public async Task<List<Banco>> FindAll()
-        {
+            {
             var bancos = await GetAll();
 
             return bancos.ToList();
-        }
+            }
 
-        public async Task<Banco?> Find(int code)
-        {
-            var banco = await Get(code);
-
-            return banco is null ? banco : new();
-        }
+        public async Task<Banco?> FindByCode(int code)
+            {
+            return await _context.Bancos.FirstOrDefaultAsync(b => b.Code == code);
+            }
         public async Task<bool> Create(Banco banco)
-        {
+            {
             await UseAsync(banco);
 
             var records = _context.SaveChanges();
 
             return records > 0;
+            }
+
+        public async Task<Banco> FindById(int id)
+            {
+            return await _context.Bancos.FirstOrDefaultAsync(b => b.Id == id);
+            }
         }
     }
-}
