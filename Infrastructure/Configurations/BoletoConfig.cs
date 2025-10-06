d0c59eecb16d68d099c539f68e16ba9c4d5cc8ee
Hursow <thuanraellombardi@gmail.com>
2025-10-06 04:47:37 -0300

    Refactor BoletoConfig for readability and consistency
    Removed unused `using` directives to reduce clutter. Renamed the `PayorCPF_CNPJ` property to `PayorCpfCnpj` for consistent naming conventions. Improved code readability by reformatting the `BoletoConfig` class and its `Configure` method with proper indentation and braces. Removed an unnecessary closing brace at the end of the file.

--- Infrastructure/Configurations/BoletoConfig.cs
+++ Infrastructure/Configurations/BoletoConfig.cs
@@ -1,27 +1,22 @@
 ﻿using Domain.Entity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure.Configurations
-{
-    public class BoletoConfig : IEntityTypeConfiguration<Boleto>
     {
-        public void Configure(EntityTypeBuilder<Boleto> bo)
+    public class BoletoConfig : IEntityTypeConfiguration<Boleto>
         {
+        public void Configure(EntityTypeBuilder<Boleto> bo)
+            {
             bo.ToTable("Boletos");
             bo.HasKey(x => x.Id);
             bo.Property(x => x.PayorName).IsRequired().HasMaxLength(50);
-            bo.Property(x => x.PayorCPF_CNPJ).HasMaxLength(14);
+            bo.Property(x => x.PayorCpfCnpj).HasMaxLength(14);
             bo.Property(x => x.PayeeName).IsRequired().HasMaxLength(50);
             bo.Property(x => x.Amount).IsRequired();
             bo.Property(x => x.DueDate).IsRequired().HasMaxLength(50);
             bo.Property(x => x.Notes).HasMaxLength(50);
             bo.Property(x => x.BancoId).IsRequired().HasMaxLength(50);
+            }
         }
     }
-}
