caa7bf73fc4b64b96b962f5def5bfdc84441c4c8
Hursow <thuanraellombardi@gmail.com>
2025-10-06 03:10:17 -0300

    refactor add DatabaseGeneratedOption

--- Domain/Entities/Banco.cs
+++ Domain/Entities/Banco.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entity
 {
     public class Banco
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         [Required]
