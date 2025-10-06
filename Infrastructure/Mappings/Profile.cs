bce142fbb5da03423ca86c3309c4c4a56d5e4f6f
Hursow <thuanraellombardi@gmail.com>
2025-10-06 04:48:08 -0300

    Refactor MapProfile and clean up unused code
    Removed unused `using` directives to improve code clarity. Reformatted the `namespace` declaration by removing unnecessary braces. Refactored the `MapProfile` constructor for cleaner formatting and readability. Simplified the file structure by removing redundant closing braces.

--- Infrastructure/Mappings/Profile.cs
+++ Infrastructure/Mappings/Profile.cs
@@ -1,20 +1,15 @@
 ﻿using AutoMapper;
 using Domain.DTOs;
 using Domain.Entity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure.Mappings
-{
-    public class MapProfile : Profile
     {
-        public MapProfile()
+    public class MapProfile : Profile
         {
+        public MapProfile()
+            {
             CreateMap<Banco, BancoDto>().ReverseMap();
             CreateMap<Boleto, BoletoDto>().ReverseMap();
+            }
         }
     }
-}
