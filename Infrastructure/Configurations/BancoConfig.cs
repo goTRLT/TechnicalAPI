ae58243df8318d28c608b71fac28beecbd7edd66
Hursow <thuanraellombardi@gmail.com>
2025-10-06 04:47:27 -0300

    Refactor BancoConfig and add unique index on Code
    Removed unused using directives to clean up the code. Adjusted
    the formatting of the BancoConfig class for better readability.
    Added a unique index on the Code property of the Banco entity
    to enforce uniqueness at the database level.

--- Infrastructure/Configurations/BancoConfig.cs
+++ Infrastructure/Configurations/BancoConfig.cs
@@ -1,23 +1,19 @@
 ﻿using Domain.Entity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure.Configurations
-{
-    public class BancoConfig : IEntityTypeConfiguration<Banco>
     {
-        public void Configure(EntityTypeBuilder<Banco> ba)
+    public class BancoConfig : IEntityTypeConfiguration<Banco>
         {
+        public void Configure(EntityTypeBuilder<Banco> ba)
+            {
             ba.ToTable("Bancos");
             ba.HasKey(x => x.Id);
             ba.Property(x => x.Name).IsRequired().HasMaxLength(50);
             ba.Property(x => x.Code).IsRequired();
             ba.Property(x => x.InterestPercent).IsRequired();
+            ba.HasIndex(x => x.Code).IsUnique();
+            }
         }
     }
-}
