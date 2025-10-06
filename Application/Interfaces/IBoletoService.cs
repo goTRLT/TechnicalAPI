54858718a41733dc788bce63d2415f7446509f98
Hursow <thuanraellombardi@gmail.com>
2025-10-06 04:50:11 -0300

    Add FindPayorDocs method and clean up IBoletoService
    Refactored `IBoletoService` interface by adding the `FindPayorDocs` method, which retrieves a `BoletoDto` based on optional `cpfCnpj` and `payorName` parameters. Removed unused `using` directives for cleaner code. Reformatted the `namespace` and interface braces for improved readability.

--- Application/Interfaces/IBoletoService.cs
+++ Application/Interfaces/IBoletoService.cs
@@ -1,16 +1,11 @@
 ﻿using Domain.DTOs;
-using Domain.Entity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Interfaces
-{
-    public interface IBoletoService
     {
+    public interface IBoletoService
+        {
         Task<BoletoDto> Find(int id);
         Task<bool> Create(BoletoDto bancoDto);
+        Task<BoletoDto?> FindPayorDocs(string? cpfCnpj, string payorName);
+        }
     }
-}
