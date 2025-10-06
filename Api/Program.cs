6ca3eb8f743802fa26100e07a5d8a29dc0901a56
Hursow <thuanraellombardi@gmail.com>
2025-10-06 04:51:16 -0300

    Refactor Program.cs for modularity and maintainability
    Refactored service registration by replacing `AddServices(connectionString)` with `AddServices()` and removed direct connection string handling. Replaced inline HTTP request pipeline configuration and route mappings with a new `Configure()` method. Removed `/bancos` and `/boletos` endpoints. Added `using Api.Extensions;` to support new extension methods.

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,36 +1,14 @@
 using Api;
+using Api.Extensions;
 using Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("TechDb");
-
-builder.Services.AddServices(connectionString);
+builder.AddServices();
 
 var app = builder.Build();
 
-app.MapGet("/bancos", async (TechDbContext context) =>
-{
-    return await context.Bancos.ToListAsync();
-});
-
-app.MapGet("/boletos", async (TechDbContext context) =>
-{
-    return await context.Boletos.ToListAsync();
-});
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.Configure();
 
 app.Run();
