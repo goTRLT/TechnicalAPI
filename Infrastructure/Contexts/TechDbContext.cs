72610d0470e05b8b4210770d27eb17ec3ad95beb
Hursow <thuanraellombardi@gmail.com>
2025-10-06 04:47:47 -0300

    Refactor TechDbContext for clarity and maintainability
    Removed unused using directives to clean up imports. Improved
    formatting of the class declaration, constructor, and methods
    for better readability. Adjusted the structure of the class
    and removed unnecessary closing braces to enhance clarity.

--- Infrastructure/Contexts/TechDbContext.cs
+++ Infrastructure/Contexts/TechDbContext.cs
@@ -1,27 +1,22 @@
 ﻿using Domain.Entity;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure.Context
-{
-    public class TechDbContext : DbContext
     {
-        public TechDbContext(DbContextOptions<TechDbContext> options) : base(options)
+    public class TechDbContext : DbContext
         {
-        }
+        public TechDbContext(DbContextOptions<TechDbContext> options) : base(options)
+            {
+            }
 
         public DbSet<Banco> Bancos => Set<Banco>();
         public DbSet<Boleto> Boletos => Set<Boleto>();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
+            {
             modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
 
             modelBuilder.HasDefaultSchema("TechDb");
+            }
         }
     }
-}
