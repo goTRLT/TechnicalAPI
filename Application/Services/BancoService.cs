5b44f53406ad8486bedc43d6a008c2abd286b948
Hursow <thuanraellombardi@gmail.com>
2025-10-06 04:50:22 -0300

    Refactor services and add new methods
    Refactored `BancoService` and `BoletoService` by removing unused `using` directives, improving formatting, and renaming `Find` to `FindByCode` in `BancoService`. Added `FindById` to `BancoService` and `FindPayorDocs` to `BoletoService` for enhanced functionality. Updated method implementations to align with repository changes.

--- Application/Services/BancoService.cs
+++ Application/Services/BancoService.cs
@@ -3,47 +3,48 @@ using AutoMapper;
 using Domain.DTOs;
 using Domain.Entity;
 using Domain.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Services
-{
-    public class BancoService : IBancoService
     {
+    public class BancoService : IBancoService
+        {
         private readonly IBancoRepository _bancoRepository;
         private readonly IMapper _mapper;
 
         public BancoService(IBancoRepository bancoRepository, IMapper mapper)
-        {
+            {
             _bancoRepository = bancoRepository;
             _mapper = mapper;
-        }
+            }
 
         public async Task<List<BancoDto>> FindAll()
-        {
+            {
             var all = new List<BancoDto>();
             var bancos = await _bancoRepository.FindAll();
             foreach (var banco in bancos)
-            {
+                {
                 all.Add(_mapper.Map<BancoDto>(banco));
-            }
+                }
             return all;
-        }
+            }
 
-        public async Task<BancoDto> Find(int code)
-        {
-            var banco = await _bancoRepository.Find(code);
+        public async Task<BancoDto> FindByCode(int code)
+            {
+            var banco = await _bancoRepository.FindByCode(code);
             return _mapper.Map<BancoDto>(banco);
-        }
+            }
 
         public async Task<bool> Create(BancoDto bancoDto)
-        {
+            {
             var bancoMap = _mapper.Map<Banco>(bancoDto);
             var success = await _bancoRepository.Create(bancoMap);
             return success;
+            }
+
+        public async Task<BancoDto> FindById(int id)
+            {
+            var banco = await _bancoRepository.FindById(id);
+            return _mapper.Map<BancoDto>(banco);
+            }
         }
     }
-}
--- Application/Services/BoletoService.cs
+++ Application/Services/BoletoService.cs
@@ -3,39 +3,40 @@ using AutoMapper;
 using Domain.DTOs;
 using Domain.Entity;
 using Domain.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Services
-{
-    public class BoletoService : IBoletoService
     {
+    public class BoletoService : IBoletoService
+        {
         private readonly IBoletoRepository _boletoRepository;
         private readonly IBancoRepository _bancoRepository;
         private readonly IMapper _mapper;
 
         public BoletoService(IBoletoRepository boletoRepository, IBancoRepository bancoRepository, IMapper mapper)
-        {
+            {
             _boletoRepository = boletoRepository;
             _bancoRepository = bancoRepository;
             _mapper = mapper;
-        }
+            }
 
         public async Task<BoletoDto> Find(int id)
-        {
+            {
             var boleto = await _boletoRepository.Find(id);
             return _mapper.Map<BoletoDto>(boleto);
-        }
+            }
 
         public async Task<bool> Create(BoletoDto boletoDto)
-        {
+            {
             var boletoMap = _mapper.Map<Boleto>(boletoDto);
             var success = await _boletoRepository.Create(boletoMap);
             return success;
+            }
+
+        public async Task<BoletoDto?> FindPayorDocs(string? cpfCnpj, string payorName)
+            {
+            var boleto = await _boletoRepository.FindPayorDocs(cpfCnpj, payorName);
+            return boleto == null ? null : _mapper.Map<BoletoDto>(boleto);
+            }
         }
     }
-}
 
