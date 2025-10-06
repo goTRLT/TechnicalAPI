c314ea673f8943b803a8b093176b336e743ce72d
Hursow <thuanraellombardi@gmail.com>
2025-10-06 04:51:41 -0300

    Add service configuration and dependency injection
    Introduced a static `Service` class in `Api.Extensions` with an `AddServices` method to configure application services. Added support for controllers, Swagger documentation (with XML comments and metadata), and PostgreSQL database integration using `TechDbContext`. Registered scoped services for `IBancoService`, `IBoletoService`, and their respective repositories. Configured AutoMapper with a mapping profile.

---
+++ Api/Extensions/Service.cs
@@ -0,0 +1,58 @@
+﻿using Application.Interfaces;
+using Application.Services;
+using Domain.Interfaces;
+using Infrastructure.Context;
+using Infrastructure.Mappings;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System.Reflection;
+
+namespace Api.Extensions
+    {
+    public static class Service
+        {
+        public static void AddServices(this WebApplicationBuilder builder)
+            {
+            var connectionString = builder.Configuration.GetConnectionString("TechDb");
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                // Add XML comments
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                if (File.Exists(xmlPath))
+                    {
+                    options.IncludeXmlComments(xmlPath);
+                    }
+
+                // Add metadata
+                options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                    {
+                    Title = "TechnicalAPI",
+                    Version = "v0.6",
+                    Description = "API for Banco and Boleto management.",
+                    Contact = new Microsoft.OpenApi.Models.OpenApiContact
+                        {
+                        Name = "Thuan Rael Lombardi",
+                        Email = "thuanraellombardi@gmail.com"
+                        }
+                    });
+            });
+
+            builder.Services
+              .AddDbContext<TechDbContext>(options => options
+              .UseNpgsql(connectionString, b => b
+              .MigrationsAssembly("Infrastructure")));
+
+            builder.Services.AddScoped<IBancoService, BancoService>();
+            builder.Services.AddScoped<IBoletoService, BoletoService>();
+            builder.Services.AddScoped<IBancoRepository, BancoRepository>();
+            builder.Services.AddScoped<IBoletoRepository, BoletoRepository>();
+
+            builder.Services.AddAutoMapper(map => map.AddProfile(new MapProfile()));
+            }
+        }
+    }
