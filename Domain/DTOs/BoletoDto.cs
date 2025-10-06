84d7b0f4f0803ef3a2af70878a656e9d3a528b19
Hursow <thuanraellombardi@gmail.com>
2025-10-06 03:11:34 -0300

    refactor remove unused usings and change property types

--- Domain/DTOs/BoletoDto.cs
+++ Domain/DTOs/BoletoDto.cs
@@ -1,10 +1,4 @@
-﻿using Domain.Entity;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Domain.DTOs
 {
@@ -18,17 +12,17 @@ namespace Domain.DTOs
         public string PayorName { get; set; }
 
         [MaxLength(14)]
-        public int? PayorCPF_CNPJ { get; set; }
+        public string? PayorCpfCnpj { get; set; }
 
         [Required]
         [MaxLength(50)]
         public string PayeeName { get; set; }
 
         [Required]
-        public int Amount { get; set; }
+        public decimal Amount { get; set; }
 
         [Required]
-        public DateOnly DueDate { get; set; }
+        public DateTime DueDate { get; set; }
 
         [MaxLength(50)]
         public string? Notes { get; set; }
